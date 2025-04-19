58331b24e3fafe5c39b90286fd5df95a4f387c50
mjpark <pmj9498@gmail.com>
2025-04-19 10:10:12 +0900

    :sparkles: [Feat] 헤더(네비)에 반응형 구현
    드랍다운 애니메이션 및 useRef를 통해 사용자 경험 개선

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import { MenuIcon } from 'lucide-react';
+import { motion, AnimatePresence } from 'framer-motion';
 import Link from 'next/link';
+import { useEffect, useRef, useState } from 'react';
 import { usePathname } from 'next/navigation';
 
 const navLinks = [
@@ -12,9 +15,30 @@ const navLinks = [
 
 export default function Header() {
   const path = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const toggleMenu = () => setIsOpen((prev) => !prev);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (
+        isOpen &&
+        !dropdownRef.current?.contains(e.target as Node) &&
+        !buttonRef.current?.contains(e.target as Node)
+      )
+        setIsOpen(false);
+    };
+
+    window.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      window.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen]);
 
   return (
-    <header className='fixed top-0 left-0 w-full flex justify-between items-center px-10 py-5 font-semibold bg-[color:var(--background)]'>
+    <header className='fixed top-0 left-0 w-full flex justify-between items-center z-10 px-10 py-5 font-semibold bg-[color:var(--background)] leading-none'>
       <Link
         href='/'
         className='text-3xl font-bold inline-block hover:[animation:var(--animation-wiggle)] hover:text-[color:var(--accent)]'
@@ -22,24 +46,63 @@ export default function Header() {
       >
         MyungJun
       </Link>
-      <div>
-        <nav>
-          <ul className='flex gap-20'>
-            {navLinks.map((link) => (
-              <li key={link.path}>
-                <Link
-                  href={link.path}
-                  className={`hover:text-[color:var(--accent)] transition ${
-                    path === link.path ? 'text-[color:var(--accent)]' : ''
-                  }`}
-                >
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
+      {/* 데스크탑 메뉴 */}
+      <div className='hidden lg:block'>
+        <ul className='flex gap-20'>
+          {navLinks.map((link) => (
+            <li key={link.path}>
+              <Link
+                href={link.path}
+                className={`hover:text-[color:var(--accent)] transition ${
+                  path === link.path ? 'text-[color:var(--accent)]' : ''
+                }`}
+              >
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
+      {/* 모바일 메뉴 아이콘 */}
+      <div className='lg:hidden'>
+        <button
+          ref={buttonRef}
+          onClick={toggleMenu}
+          className='hover:text-[color:var(--accent)] transition'
+        >
+          <MenuIcon size={28} />
+        </button>
+      </div>
+      {/* 모바일 메뉴 드랍다운 */}
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            key='dropdown-menu'
+            ref={dropdownRef}
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.5, ease: 'easeInOut' }}
+            className='absolute top-full left-0 w-full bg-[color:var(--background)] px-10 py-5 border-b-1 border-[color:var(--border)] shadow-lg rounded-b-lg lg:hidden'
+          >
+            <ul className='flex flex-col gap-8'>
+              {navLinks.map((link) => (
+                <li key={link.path}>
+                  <Link
+                    href={link.path}
+                    onClick={() => setIsOpen(false)}
+                    className={`hover:text-[color:var(--accent)] transition ${
+                      path === link.path ? 'text-[color:var(--accent)]' : ''
+                    }`}
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </header>
   );
 }
